82dcfd5c6389bf592821eeaeb0ca252017a47ef2
boikopetro <boikopetromail@gmail.com>
2020-12-02 20:04:14 +0000

    add regex validation

--- index.js
+++ index.js
@@ -3,4 +3,21 @@ let maskOption = {
     mask: "+38(000)000-00-00",
     lazy: false
 }
-let mask = new IMask(element, maskOption)
+let mask = new IMask(element, maskOption);
+
+document.getElementById('btn').addEventListener('click', checkValidation);
+
+function checkValidation(){
+    const clientPhone = document.getElementById('phone').value;
+    const regex = /^\+38\(0\d{2}\)\d{3}\-\d{2}\-\d{2}/;
+    const validation = regex.test(clientPhone);
+    if(clientPhone==="+38(___)___-__-__"){
+        alert('enter your phone number')
+    } else if (validation) {
+        alert('ok')
+    } else {
+        alert(clientPhone+" is not correct  :( ")
+    }
+}
+
+
